6f8aac0e56bd9a0381487d9a8201c4c4bf6bed9d
pavankumarvanam <v.pavan@ailabs.in>
2020-07-31 17:24:57 +0530

    Updates

---
+++ precache-manifest.f30c9d7e8c0b07c8ef1f3f02cb5b7f98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f223210091156a6c46b389f815acd59",
+    "url": "/github-api/index.html"
+  },
+  {
+    "revision": "0813e499b683eb24001a",
+    "url": "/github-api/static/css/main.61006cb4.chunk.css"
+  },
+  {
+    "revision": "3e31a98592c2a001881b",
+    "url": "/github-api/static/js/2.18277e96.chunk.js"
+  },
+  {
+    "revision": "73c7aa71bed530afae86738bff3c3a15",
+    "url": "/github-api/static/js/2.18277e96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0813e499b683eb24001a",
+    "url": "/github-api/static/js/main.41565fe9.chunk.js"
+  },
+  {
+    "revision": "76d8c201154c85256f49",
+    "url": "/github-api/static/js/runtime-main.21ed75d5.js"
+  }
+]);
